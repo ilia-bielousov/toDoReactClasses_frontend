9c93ec0f2db9c2b48c4c683caf1eb67975832596
ilia-bielousov <ilia.bielousov@gmail.com>
2023-05-17 17:48:37 +0200

    created Component AppList

--- my-app/src/components/app/App.js
+++ my-app/src/components/app/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import AppHeader from '../appHeader/AppHeader';
 import AppNav from '../appNav/AppNav';
 import AppForm from '../appForm/AppForm';
+import AppList from '../appList/AppList';
 import './App.scss';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
         <AppHeader />
         <AppNav />
         <AppForm />
+        <AppList />
       </div>
     );
   }
---
+++ my-app/src/components/appList/AppList.js
@@ -0,0 +1,144 @@
+import { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import './AppList.scss';
+
+class AppList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Container>
+        <div className="main__list list p-3 mb-4">
+          <Row>
+            <ul className="list__inner">
+              <li className="list__item d-flex align-items-center">
+                <Col sm={1} xs={2} className="d-grid">
+                  <span class="list__done">
+                    <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <g clip-path="url(#clip0_116_188)">
+                        <path
+                          d="M38.557 21.834L23.4 36.991L15.143 28.757L11.9 32L23.4 43.5L41.8 25.1L38.557 21.834ZM28 9C15.304 9 5 19.304 5 32C5 44.696 15.304 55 28 55C40.696 55 51 44.696 51 32C51 19.304 40.696 9 28 9ZM28 50.4C17.834 50.4 9.6 42.166 9.6 32C9.6 21.834 17.834 13.6 28 13.6C38.166 13.6 46.4 21.834 46.4 32C46.4 42.166 38.166 50.4 28 50.4Z"
+                          fill="#D98326" />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_116_188">
+                          <rect width="64" height="64" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </span>
+                </Col>
+                <Col sm={10} xs={8} className="d-grid">
+                  <p class="list__text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci perspiciatis natus impedit!
+                    Doloribus, molestiae aut!
+                  </p>
+                </Col>
+                <Col sm={1} xs={2} className="d-grid">
+                  <span class="list__delete">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <g opacity="0.3" clip-path="url(#clip0_116_185)">
+                        <path
+                          d="M7.99999 25.3333C7.99999 26.8 9.19999 28 10.6667 28H21.3333C22.8 28 24 26.8 24 25.3333V9.33333H7.99999V25.3333ZM10.6667 12H21.3333V25.3333H10.6667V12ZM20.6667 5.33333L19.3333 4H12.6667L11.3333 5.33333H6.66666V8H25.3333V5.33333H20.6667Z"
+                          fill="#B30B04" />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_116_185">
+                          <rect width="32" height="32" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </span>
+                </Col>
+              </li>
+              <li className="list__item d-flex align-items-center">
+                <Col sm={1} xs={2} className="d-grid">
+                  <span class="list__done">
+                    <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <g clip-path="url(#clip0_116_188)">
+                        <path
+                          d="M38.557 21.834L23.4 36.991L15.143 28.757L11.9 32L23.4 43.5L41.8 25.1L38.557 21.834ZM28 9C15.304 9 5 19.304 5 32C5 44.696 15.304 55 28 55C40.696 55 51 44.696 51 32C51 19.304 40.696 9 28 9ZM28 50.4C17.834 50.4 9.6 42.166 9.6 32C9.6 21.834 17.834 13.6 28 13.6C38.166 13.6 46.4 21.834 46.4 32C46.4 42.166 38.166 50.4 28 50.4Z"
+                          fill="#D98326" />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_116_188">
+                          <rect width="64" height="64" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </span>
+                </Col>
+                <Col sm={10} xs={8} className="d-grid">
+                  <p class="list__text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci perspiciatis natus impedit!
+                    Doloribus, molestiae aut!
+                  </p>
+                </Col>
+                <Col sm={1} xs={2} className="d-grid">
+                  <span class="list__delete">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <g opacity="0.3" clip-path="url(#clip0_116_185)">
+                        <path
+                          d="M7.99999 25.3333C7.99999 26.8 9.19999 28 10.6667 28H21.3333C22.8 28 24 26.8 24 25.3333V9.33333H7.99999V25.3333ZM10.6667 12H21.3333V25.3333H10.6667V12ZM20.6667 5.33333L19.3333 4H12.6667L11.3333 5.33333H6.66666V8H25.3333V5.33333H20.6667Z"
+                          fill="#B30B04" />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_116_185">
+                          <rect width="32" height="32" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </span>
+                </Col>
+              </li>
+              <li className="list__item d-flex align-items-center">
+                <Col sm={1} xs={2} className="d-grid">
+                  <span class="list__done">
+                    <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <g clip-path="url(#clip0_117_236)">
+                        <path
+                          d="M28 9C15.304 9 5 19.304 5 32C5 44.696 15.304 55 28 55C40.696 55 51 44.696 51 32C51 19.304 40.696 9 28 9ZM28 50.4C17.834 50.4 9.6 42.166 9.6 32C9.6 21.834 17.834 13.6 28 13.6C38.166 13.6 46.4 21.834 46.4 32C46.4 42.166 38.166 50.4 28 50.4Z"
+                          fill="#737373" />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_117_236">
+                          <rect width="64" height="64" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </span>
+                </Col>
+                <Col sm={10} xs={8} className="d-grid">
+                  <p class="list__text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci perspiciatis natus impedit!
+                    Doloribus, molestiae aut!
+                  </p>
+                </Col>
+                <Col sm={1} xs={2} className="d-grid">
+                  <span class="list__delete">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <g opacity="1" clip-path="url(#clip0_116_185)">
+                        <path
+                          d="M7.99999 25.3333C7.99999 26.8 9.19999 28 10.6667 28H21.3333C22.8 28 24 26.8 24 25.3333V9.33333H7.99999V25.3333ZM10.6667 12H21.3333V25.3333H10.6667V12ZM20.6667 5.33333L19.3333 4H12.6667L11.3333 5.33333H6.66666V8H25.3333V5.33333H20.6667Z"
+                          fill="#B30B04" />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_116_185">
+                          <rect width="32" height="32" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </span>
+                </Col>
+              </li>
+            </ul>
+          </Row>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default AppList;
